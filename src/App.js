c4363962a200a6412d2ea6f3150764a827af2ddc
Julian Layen <layenjulian@gmail.com>
2025-02-04 14:08:18 +0100

    feat : HTML templates

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import New from "./components/New";
+import Game from "./components/Game";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        {/*
+            <div className="nav-bar">
+              <Link to="/">Login</Link>
+              <Link to="/register">Register</Link>
+              <Link to="/lancer-partie">Lancer une Partie</Link>
+              <Link to="/jeu">Jeu</Link>
+            </div>
+        */}
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/lancer-partie" element={<New />} />
+          <Route path="/jeu" element={<Game />} />
+        </Routes>
+      </Router>
   );
 }
 
