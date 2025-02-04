46e3ce9037548aac578a932cac9e969ac710d950
thibault_f <78164277+thibault-forret@users.noreply.github.com>
2025-02-04 16:26:41 +0100

    background switch component

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,17 +1,20 @@
 import React, { useState, useEffect } from "react";
 import Clock from "./Clock";
 import "../game.css";
+import backgroundDay from "../assets/images/background-day.png";
+import backgroundNight from "../assets/images/background-night-2.png";
 
 function Jeu() {
-    const [backgroundImage, setBackgroundImage] = useState("./assets/images/background-day.png");
+    const [backgroundImage, setBackgroundImage] = useState(backgroundDay);
 
     useEffect(() => {
         const updateBackground = () => {
             const now = new Date();
             let minutes = now.getMinutes();
-            setBackgroundImage(minutes % 2 === 0 ? "./assets/images/background-day.png" : "./assets/images/background-night-2.png");
+            setBackgroundImage(minutes % 2 === 0 ? backgroundDay : backgroundNight);
         };
 
+        updateBackground();
         const interval = setInterval(updateBackground, 1000);
         return () => clearInterval(interval); // Nettoyage de l'intervalle
     }, []);
